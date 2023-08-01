d4f9eecd847bcc17aa72df0e38c5ff70dcfe3379
Yog <Yog@LAPTOP-M3K35D9B.twmi.rr.com>
2023-08-01 19:10:38 -0400

    added page locator and methods

--- src/main/java/com/acti/page/Enterpage.java
+++ src/main/java/com/acti/page/Enterpage.java
@@ -1,5 +1,7 @@
 package com.acti.page;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.acti.base.Driverscript;
@@ -12,15 +14,31 @@ reviewed by:-
 
 public class Enterpage extends Driverscript{
 	//**************************page Locator**********************//
-	
-	
+	@FindBy(id="logoutLink") WebElement logoutLink;
+	@FindBy(xpath="//a[class='userProfileLink username']") WebElement userloggedText;
+	@FindBy(xpath="//div[@id='container_tasks']')") WebElement menuText;
+	@FindBy(xpath="//div[@id='container_reports']") WebElement menuReport;
+	@FindBy(xpath="//div[@id='container_users']") WebElement menuUser;
 	
 	//**************************page Initalization**********************//
 	
 	public Enterpage() {
 		PageFactory.initElements(driver, this);
 	
-	
+	}
 
 	//**************************page Method**********************//
+		
+		public void clickLogOut() {
+			logoutLink.click();
+	
+}
+		public String getUserLoggedIn() {
+			return userloggedText.getText();
+		}
+		
+		public void clickTaskMenu() {
+			menuText.click();
+			
+		}
 }
