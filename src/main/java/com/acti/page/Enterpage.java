aac8a6f1614aa22debe828e7899cdd732bcef212
Yog <Yog@LAPTOP-M3K35D9B.twmi.rr.com>
2023-08-01 17:45:42 -0400

    added author to enter page

--- src/main/java/com/acti/page/Enterpage.java
+++ src/main/java/com/acti/page/Enterpage.java
@@ -2,6 +2,12 @@ package com.acti.page;
 
 import com.acti.base.Driverscript;
 
+/* Name Enter page
+Description= manage the locator and methods of enter page
+developed by:-
+reviewed by:-
+*/
+
 public class Enterpage extends Driverscript{
 
 }
