c5e4aed9bb4759c75a6b120bfb530885fd9d9d6b
Yog <Yog@LAPTOP-M3K35D9B.twmi.rr.com>
2023-08-01 19:18:18 -0400

    added object of Enter page in Base test

--- src/test/java/com/acti/testcase/Basetest.java
+++ src/test/java/com/acti/testcase/Basetest.java
@@ -4,16 +4,19 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.acti.base.Driverscript;
+import com.acti.page.Enterpage;
 import com.acti.page.Loginpage;
 
 public class Basetest extends Driverscript {
 	 
 	Loginpage lp ;
+	Enterpage ep ;
 		
 		@BeforeMethod
 		public void pretest() {
 			initApplication();
 			lp = new Loginpage();
+			ep=new Enterpage();
 		}
 		
 		@AfterMethod
